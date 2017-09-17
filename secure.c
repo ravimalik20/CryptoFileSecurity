b29bfbe1cd95587c45c8252c4a83220de733c4bb
Ravi Malik <ravimalik2364@gmail.com>
2017-09-17 01:41:56 -0400

    Updated submodules to latest versions.

--- secure.c
+++ secure.c
@@ -20,27 +20,44 @@
 *==========================================================================================*/
 
 #include<stdio.h>
-#include"CryptoLibAES-128/encrypt_file.h"
-#include"password_tools.h"
 #include<string.h>
+#include"CryptoLibAES-128/aes.h"
+#include"CryptoLibSHA-256/hash_string.h"
+
+void hash_password(char *str, unsigned char *pass)
+{
+	struct hash hs;
+	hs=hash_string(str);
+	char hex_str[65];
+	hash_to_hex_string(&hs,hex_str);
+	unsigned char pwd[17];
+	int i;
+	for(i=0;i<16;i++)
+	{	pwd[i]=hex_str[i];
+	}
+
+	for(i=0;i<17;i++)
+	{	pass[i]=pwd[i];
+	}
+}
 
 void main(int argc, char* argv[])
-{	FILE *fp_i,*fp_o;
+{
+	FILE *fp_i,*fp_o;
 	fp_i=fopen(argv[2],"rb");
 	fp_o=fopen(argv[4],"wb");
-	unsigned char pwd[17];	
-	struct block key;	
-	hash_password(argv[3],pwd);
-	key=prepare_key(pwd);
+
+	char pwd[16];
+
+	hash_password(argv[3], pwd);
+
 	if(!strcmp(argv[1],"-e"))
-	{	encrypt_file(fp_i,key,fp_o);
-	}
+		aes_encrypt_file(fp_i, fp_o, pwd);
 	else if(!strcmp(argv[1],"-d"))
-	{	decrypt_file(fp_i,key,fp_o);
-	}
+		aes_decrypt_file(fp_i, fp_o, pwd);
 	else
-	{	printf("Please enter correct mode of operation (-e/-d).");
-	}
+		printf("Please enter correct mode of operation (-e/-d).");
+
 	fclose(fp_i);
 	fclose(fp_o);
 }
